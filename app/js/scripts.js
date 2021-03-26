b138fe0265f825c523673321006d287be0329c9e
Seryoga <main.email.seryoga@gmail.com>
2021-03-26 03:14:55 +0200

    Initial commit

---
+++ app/js/scripts.js
@@ -0,0 +1,82 @@
+'use strict';
+
+$(document).ready(function() {
+
+  // якір
+  $(".go-to").on('click',function(e){
+
+    e.preventDefault();
+
+    var anchor = $(this).attr("href");
+
+    if ($(anchor).length) {
+      var run = $(anchor).offset().top;
+      $('body,html').stop().animate({scrollTop: run}, 1500);
+    } else {
+      console.warn("ID don't search!")
+    }
+  });
+
+  $(".mobile-menu").on("click", function(){
+    $(this).toggleClass("active");
+    $('header .title').toggleClass("active-style");
+    $("header .menu").stop().slideToggle( "slow", function(){
+      if ($(this).css('display') === 'none'){
+        $(this).removeAttr('style');
+      }
+    });
+  });
+
+  $.fn.forceNumbericOnly = function () {
+    return this.each(function () {
+      $(this).keydown(function (e) {
+        var key = e.charCode || e.keyCode || 0;
+        return (key == 8 || key == 9 || key == 46 || (key >= 37 && key <= 40) || (key >=
+          48 && key <= 57) || (key >= 96 && key <= 105) || key == 107 || key ==
+          109 || key == 173 || key == 61);
+      });
+    });
+  };
+
+  $('input[type=tel]').forceNumbericOnly();
+
+  $('.partners').slick({
+    infinite: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          dots: true,
+          arrows: false,
+        }
+      },
+      {
+        breakpoint: 350,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+        }
+      }
+    ]
+  });
+
+});
